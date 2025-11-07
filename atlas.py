ef7143a1d55f94cd44c0dee9df3c93ce0443d4ce
Alejandro Hita <alejandrohitagarrido@gmail.com>
2025-11-07 16:04:00 +0100

    El mapa

--- atlas.py
+++ atlas.py
@@ -1,6 +1,6 @@
 import pyxel
 pyxel.init(500, 500)
 pyxel.load('my_resource.pyxres')
-pyxel.images[1].load(0, 0, 'atlas1.png')
-pyxel.images[2].load(0, 0, 'atlas2.png')
+pyxel.images[1].load(0, 0, 'mapa.png')
+pyxel.images[2].load(0, 0, 'objetos.png')
 pyxel.save('my_resource.pyxres')
--- main.py
+++ main.py
@@ -1,67 +1,55 @@
 import pyxel
 
 class Imagen:
-    def __init__(self, posición: tuple[int, int], tamaño: tuple[int, int]) -> None:
-        self.x, self.y = posición
-        self.ancho, self.alto = tamaño
+    def __init__(self, imagen: int, posicion: tuple[int, int], tamano: tuple[int, int]) -> None:
+        self.x, self.y = posicion
+        self.ancho, self.alto = tamano
+        self.imagen = imagen
 
-    def renderizar(self, posición: tuple[int, int]) -> None:
-        x, y = posición
-        pyxel.blt(x, y, 0, self.x, self.y, self.ancho, self.alto)
+    def renderizar(self, posicion: tuple[int, int]) -> None:
+        x, y = posicion
+        pyxel.blt(x, y, self.imagen, self.x, self.y, self.ancho, self.alto)
 
 class Jugador:
-    def __init__(self, posición: int, imagen: Imagen, escalones: list[int], teclas) -> None:
-        self.x = posición
+    def __init__(self, posicion: int, imagen: Imagen, escalones: list[int], teclas) -> None:
+        self.x = posicion
         self.y = 0
         self.imagen = imagen
         self.escalones = escalones
-        self.posición = 0
+        self.posicion = 0
         self.arriba, self.abajo = teclas
 
     def actualizar(self) -> None:
         n = len(self.escalones)
         if pyxel.btnp(self.arriba):
-            self.posición = min(self.posición + 1, n - 1)
+            self.posicion = min(self.posicion + 1, n - 1)
         elif pyxel.btnp(self.abajo):
-            self.posición = max(self.posición - 1, 0)
+            self.posicion = max(self.posicion - 1, 0)
 
-        self.y = self.escalones[self.posición]
-
-    def renderizar(self) -> None:
-        self.imagen.renderizar((self.x, self.y))
-
-class Paquete:
-    def __init__(self, posición: tuple[int, int], imagen: Imagen) -> None:
-        self.x, self.y = posición
-        self.imagen = imagen
-
-    def actualizar(self) -> None:
-        pass
+        self.y = self.escalones[self.posicion]
 
     def renderizar(self) -> None:
         self.imagen.renderizar((self.x, self.y))
 
-MARIO: Imagen = Imagen((0, 0), (16, 16))
-LUIGI: Imagen = Imagen((0, 17), (16, 16))
+MAPA = Imagen(1, (0, 12), (240, 136))
+MARIO: Imagen = Imagen(0, (0, 0), (16, 16))
+LUIGI: Imagen = Imagen(0, (0, 17), (16, 16))
 
 class Partida:
-    def __init__(self) -> None:
-        self.mario = Jugador(900, MARIO, [400, 300, 200, 100], (pyxel.KEY_UP, pyxel.KEY_DOWN))
-        self.luigi = Jugador(100, LUIGI, [400, 300, 200, 100], (pyxel.KEY_W, pyxel.KEY_S))
-        pyxel.init(1000, 500)
+    def __init__(self, mapa: Imagen) -> None:
+        self.mapa = mapa
+        
+        pyxel.init(mapa.ancho, mapa.alto)
         pyxel.load('my_resource.pyxres')
         pyxel.run(self.actualizar, self.dibujar)
 
     def actualizar(self) -> None:
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
-        self.mario.actualizar()
-        self.luigi.actualizar()
 
     def dibujar(self) -> None:
         pyxel.cls(0)
-        self.mario.renderizar()
-        self.luigi.renderizar()
+        self.mapa.renderizar((0, 0))
 
 if __name__ == '__main__':
-    Partida()
+    Partida(MAPA)
