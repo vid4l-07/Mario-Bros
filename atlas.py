4446398a9077d5aff50cd5e163a34da69fdad6d3
Alejandro Hita <alejandrohitagarrido@gmail.com>
2025-11-09 19:17:59 +0100

    Mejores assets

--- atlas.py
+++ atlas.py
@@ -1,6 +1,7 @@
 import pyxel
 pyxel.init(500, 500)
 pyxel.load('my_resource.pyxres')
+pyxel.images[0].load(0, 0, 'mapa_objetos.png')
 pyxel.images[1].load(0, 0, 'mapa.png')
 pyxel.images[2].load(0, 0, 'objetos.png')
 pyxel.save('my_resource.pyxres')
