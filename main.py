663007b4b944dffa6f5b8bd41c823df946a3ee8f
Alejandro <71976518+LentejaRabiosa@users.noreply.github.com>
2025-12-02 16:13:47 +0100

    Clase Cinta simplificada

--- main.py
+++ main.py
@@ -84,31 +84,24 @@ class Posicion:
     def draw(self):
         self.imagen.draw(self.posicion)
 
+# TODO: Imagen de caida
 class Cinta:
-    # TODO: Hay que simplificar mucho esta clase. No es necesario pero renta
-
+    imagen_paquete: Imagen
     paquetes: list[int]
-    estados: list[Posicion]
+    inicio: tuple[int, int]
     velocidad: float
     frame: int
+    direccion: int
+    pasos: int
 
-    def __init__(self, paquete: Imagen, paquete_caida: Imagen | None, referencia: tuple[int, int], numero: int, direccion: bool, factor: float = 1):
+    def __init__(self, imagen_paquete: Imagen, inicio: tuple[int, int], pasos: int, direccion: int, factor_velocidad: float = 1):
+        self.imagen_paquete = imagen_paquete
         self.paquetes = []
-        self.velocidad = 20 / factor
-        self.estados = [Posicion(paquete, referencia)]
+        self.inicio = inicio
+        self.direccion = direccion
+        self.pasos = pasos
+        self.velocidad = 20 / factor_velocidad
         self.frame = 0
-
-        if paquete_caida != None:
-            numero -= 1
-
-        desplazamiento = 11 if direccion else -11
-        for _ in range(numero - 1):
-            x, y = self.estados[-1].posicion
-            self.estados.append(Posicion(paquete, (x + desplazamiento, y)))
-
-        if paquete_caida != None:
-            x, y = self.estados[-1].posicion
-            self.estados.append(Posicion(paquete_caida, (x + desplazamiento, y)))
 
     def añadir_paquete(self):
         self.paquetes.append(0)
@@ -121,14 +114,12 @@ class Cinta:
         self.velocidad = 20 / factor
 
     def draw(self):
+        x, y = self.inicio
         for paquete in self.paquetes:
-            if paquete < len(self.estados):
-                self.estados[paquete].draw()
+            self.imagen_paquete.draw((x + (paquete * 11 * self.direccion), y))
 
-    def caida(self):
-        if len(self.paquetes) > 0 and self.paquetes[0] == len(self.estados) - 1:
-            return True
-        return False
+    def caida(self) -> bool:
+        return any(paso >= self.pasos for paso in self.paquetes)
 
     def avanzar(self):
         self.frame += 1
@@ -141,7 +132,7 @@ class Cinta:
 
         for paquete in self.paquetes:
             nuevo = paquete + 1
-            if nuevo < len(self.estados):
+            if nuevo < self.pasos:
                 nuevos.append(nuevo)
             else:
                 salida = True
@@ -153,36 +144,32 @@ MARIO_1_1 = Imagen(2, (18, 152), (27, 26))
 MARIO_1_2 = Imagen(2, (44, 152), (27, 26))
 
 PAQUETE_1 = Imagen(2, (135, 116), (9, 4))
-PAQUETE_1_CAIDA = Imagen(2, (9, 152), (9, 9))
-
 PAQUETE_2 = Imagen(2, (109, 113), (9, 7))
-PAQUETE_2_CAIDA = Imagen(2, (75, 114), (10, 10))
-
 PAQUETE_3 = Imagen(2, (151, 94), (9, 7))
-PAQUETE_3_CAIDA = Imagen(2, (162, 94), (10, 11))
-
 PAQUETE_4 = Imagen(2, (109, 73), (9, 9))
-PAQUETE_4_CAIDA = Imagen(2, (0, 164), (11, 12))
-
 PAQUETE_5 = Imagen(2, (136, 36), (9, 9))
-PAQUETE_5_CAIDA = Imagen(2, (0, 184), (12, 12))
-
 PAQUETE_6 = Imagen(2, (109, 36), (9, 9))
-PAQUETE_6_CAIDA = Imagen(2, (16, 184), (13, 13))
 
-# Esto es una porquería
-cinta10 = Cinta(paquete=PAQUETE_6, paquete_caida=PAQUETE_6_CAIDA,referencia=(106, 28),  numero=4, direccion=False)
-cinta9 = Cinta(paquete=PAQUETE_5, paquete_caida=None,            referencia=(155, 28),  numero=4, direccion=False)
-cinta8 = Cinta(paquete=PAQUETE_5, paquete_caida=PAQUETE_5_CAIDA, referencia=(126, 47),  numero=4, direccion=True)
-cinta7 = Cinta(paquete=PAQUETE_4, paquete_caida=None,            referencia=(77,  46),  numero=4, direccion=True)
-cinta6 = Cinta(paquete=PAQUETE_4, paquete_caida=PAQUETE_4_CAIDA, referencia=(106, 65),  numero=4, direccion=False)
-cinta5 = Cinta(paquete=PAQUETE_3, paquete_caida=None,            referencia=(155, 67),  numero=4, direccion=False)
-cinta4 = Cinta(paquete=PAQUETE_3, paquete_caida=PAQUETE_3_CAIDA, referencia=(126, 86),  numero=4, direccion=True)
-cinta3 = Cinta(paquete=PAQUETE_2, paquete_caida=None,            referencia=(77,  86),  numero=4, direccion=True)
-cinta2 = Cinta(paquete=PAQUETE_2, paquete_caida=PAQUETE_2_CAIDA, referencia=(106, 105), numero=4, direccion=False)
-cinta1 = Cinta(paquete=PAQUETE_1, paquete_caida=None,            referencia=(154, 108), numero=4, direccion=False, factor=2)
-cinta0 = Cinta(paquete=PAQUETE_1, paquete_caida=PAQUETE_1_CAIDA, referencia=(217, 108), numero=3, direccion=False)
+# PAQUETE_1_CAIDA = Imagen(2, (9, 152), (9, 9))
+# PAQUETE_2_CAIDA = Imagen(2, (75, 114), (10, 10))
+# PAQUETE_3_CAIDA = Imagen(2, (162, 94), (10, 11))
+# PAQUETE_4_CAIDA = Imagen(2, (0, 164), (11, 12))
+# PAQUETE_5_CAIDA = Imagen(2, (0, 184), (12, 12))
+# PAQUETE_6_CAIDA = Imagen(2, (16, 184), (13, 13))
 
+cinta10 = Cinta(PAQUETE_6, (106, 28), 4, -1)
+cinta9 = Cinta(PAQUETE_5, (155, 28),  4, -1)
+cinta8 = Cinta(PAQUETE_5, (126, 47),  4, 1)
+cinta7 = Cinta(PAQUETE_4, (77,  46),  4, 1)
+cinta6 = Cinta(PAQUETE_4, (106, 65),  4, -1)
+cinta5 = Cinta(PAQUETE_3, (155, 67),  4, -1)
+cinta4 = Cinta(PAQUETE_3, (126, 86),  4, 1)
+cinta3 = Cinta(PAQUETE_2, (77,  86),  4, 1)
+cinta2 = Cinta(PAQUETE_2, (106, 105), 4, -1)
+cinta1 = Cinta(PAQUETE_1, (154, 108), 4, -1, 2)
+cinta0 = Cinta(PAQUETE_1, (217, 108), 3, -1)
+
+cinta0.añadir_paquete()
 cintas = [cinta0, cinta1, cinta2, cinta3, cinta4, cinta5, cinta6, cinta7, cinta8, cinta9, cinta10]
 
 class Partida:
